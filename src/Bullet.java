a7bb802e52ae65ff4cd0034925efe757477952e0
Cody Nichols <canichols2@gmail.com>
2017-04-16 04:13:45 -0600

    Initial Classes
    These are the primitive classes for the project.

---
+++ src/Bullet.java
@@ -0,0 +1,28 @@
+
+public class Bullet extends FlyingObject {
+	public Bullet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Bullet(double x, double y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Bullet(double x, double y, double dx, double dy) {
+		super(x, y, dx, dy);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Bullet(double x, double y, double dx, double dy, double rotation) {
+		super(x, y, dx, dy, rotation);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Bullet(double x, double y, double dx, double dy, double rotation, double thrust) {
+		super(x, y, dx, dy, rotation, thrust);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/Rock.java
@@ -0,0 +1,33 @@
+
+public class Rock extends FlyingObject {
+
+	public Rock() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Rock(double x, double y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Rock(double x, double y, double dx, double dy) {
+		super(x, y, dx, dy);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Rock(double x, double y, double dx, double dy, double rotation) {
+		super(x, y, dx, dy, rotation);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Rock(double x, double y, double dx, double dy, double rotation, double thrust) {
+		super(x, y, dx, dy, rotation, thrust);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void update(){
+		super.update();
+//		Continue to spin
+		this.addRotation(5.0);
+	}
+}
